0b2d4cc0c7808e99f6eddd2af142cd58064058cc
linucksrox <ericdaly.aplus@gmail.com>
2016-08-23 07:23:34 -0400

    Updated something

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -1,3 +1,4 @@
+<<<<<<< 075ab91e36cf1915854bfaafc022a86d36ff436c
 /*
  * Copyright (C) 2016 The Android Open Source Project
  *
@@ -26,3 +27,54 @@ public class PhrasesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_phrases);
     }
 }
+=======
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class PhrasesActivity extends AppCompatActivity {
+
+    ArrayList<Word> words;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.word_list);
+
+        words = new ArrayList<>();
+        words.add(new Word("Where are you going?", "minto wuksus"));
+        words.add(new Word("What is your name?", "tinnә oyaase'nә"));
+        words.add(new Word("My name is...", "oyaaset..."));
+        words.add(new Word("How are you feeling?", "michәksәs?"));
+        words.add(new Word("I'm feeling good.", "kuchi achit"));
+        words.add(new Word("Are you coming?", "әәnәs'aa?"));
+        words.add(new Word("Yes, I'm coming.", "hәә’ әәnәm"));
+        words.add(new Word("I'm coming.", "әәnәm"));
+        words.add(new Word("Let's go.", "yoowutis"));
+        words.add(new Word("Come here.", "әnni'nem"));
+
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+    }
+}
+>>>>>>> Populate data in all activities
