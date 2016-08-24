52c15bf71311dcff76ac9d6ddac42ce91befed2b
linucksrox <ericdaly.aplus@gmail.com>
2016-08-23 21:17:41 -0400

    Set correct image resource in WordAdapter

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -36,6 +37,9 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         TextView miwokTranslationTextView = (TextView) listItemView.findViewById(R.id.miwok_translation);
         miwokTranslationTextView.setText(currentWord.getMiwokTranlation());
+
+        ImageView imageIcon = (ImageView) listItemView.findViewById(R.id.image);
+        imageIcon.setImageResource(currentWord.getImageResourceID());
 
         return listItemView;
     }
